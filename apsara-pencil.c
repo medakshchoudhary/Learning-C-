f2d7fd229c98fd97df5cf8a6908c684cb1ad8c54
Daksh <ch.daksh01@gmail.com>
2023-09-29 22:13:54 +0530

    New programs Made in C Class

--- apsara-pencil.c
+++ apsara-pencil.c
@@ -7,10 +7,19 @@ int main() {
     scanf("%d", &marks);
     printf("Is the hanwriting good or not? press y for yes and n for no : ");
     scanf("%c%c", &x,&x); // two %c because enter after entering the marks is also considered as a char and we will not be able to process the next scanf
+
+    // without else 
+
+    // if(x =='y' || x == 'Y')
+    // {printf("Extra marks for good handwriting : %d",marks+10);}
+    // else if (x =='n' || x == 'N')
+    // { printf("Marks obtained for bad handwriting : %d",marks);}
+
+    // with else only
+
     if(x =='y' || x == 'Y')
     {printf("Extra marks for good handwriting : %d",marks+10);}
-    else if(x =='n' || x == 'N')
-    { printf("Marks obtained : %d",marks);}
+    else (printf("Marks obtained for bad handwriting : %d",marks-5));
 
     return 0;
 }
---
+++ divisibleBy3or5orboth.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int numberEntered;
+    printf("Enter a number to check divisibility for 3 or 5 or both : ");
+    scanf("%d", &numberEntered);
+    if (numberEntered%3 == 0)
+    {printf("Hi");} 
+    if (numberEntered%5 == 0)
+    {printf("Hello");} // only two ifs not an else if because then we can check for 15 logical thinking is applied more 
+
+    // if (1){ // this 1 makes if true automatically
+    //     (numberEntered%3==0)?((numberEntered%5==0)?printf("HiHello"):printf("Hi")):printf("Hello");
+    // } //ternary operator
+    return 0;
+}
